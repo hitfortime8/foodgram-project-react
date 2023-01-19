325c4c7c07e25bbf4ee3e0901df4dd52c51e4935
Александр Котов <hitfortime8@gmail.com>
2023-01-19 19:02:47 +0300

    9

--- backend/foodgram/users/validators.py
+++ backend/foodgram/users/validators.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 
 class ValidateMe:
 
+    def __init__(self, fields):
+        self.fields = fields
+
     def __call__(self, value):
         if value['username'].lower() == 'me':
             raise serializers.ValidationError(
